5a3839db858476ada2d48767dfec99d226ae83d7
Nihir <nihirvedd@gmail.com>
2021-04-28 14:02:52 +0100

    latent variable code + environment file

--- layers.py
+++ layers.py
@@ -4,6 +4,61 @@ from fairseq.models import transformer
 import torch
 
 
+class Latent(nn.Module):
+    def __init__(self, args, rank3=False, dropout=0):
+        super(Latent, self).__init__()
+
+        self.args = args
+
+        latent_layer_network_architecture = nn.Sequential(
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(args.latent_dim*2, args.latent_dim*2),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(args.latent_dim*2, args.latent_dim*2)
+        )
+
+        self.mean_logvar_prior = nn.Sequential(
+            nn.Linear(args.hidden_dim, args.latent_dim*2),
+            *copy.deepcopy(latent_layer_network_architecture)
+
+        )
+        self.mean_logvar_posterior = nn.Sequential(
+            nn.Linear(args.hidden_dim*2, args.latent_dim*2),
+            *copy.deepcopy(latent_layer_network_architecture)
+        )
+
+    def forward(self, x, x_p):
+        mean_logvar_prior = self.mean_logvar_prior(x)
+        mean_prior, logvar_prior = mean_logvar_prior[:, :self.args.latent_dim], mean_logvar_prior[:, self.args.latent_dim:]
+
+        eps = torch.randn(mean_prior.size()).to(self.args.device)
+        std = torch.exp(0.5 * logvar_prior)
+        z = eps * std + mean_prior
+        kld_loss = 0
+
+        mean_posterior, logvar_posterior = None, None
+        if x_p is not None:  # if x_p IS None, then we're in eval mode.
+            mean_logvar_posterior = self.mean_logvar_posterior(torch.cat((x_p, x), dim=-1))
+            mean_posterior, logvar_posterior = mean_logvar_posterior[:, :self.args.latent_dim], mean_logvar_posterior[:, self.args.latent_dim:]
+
+            kld_loss = gaussian_kld(mean_posterior, logvar_posterior, mean_prior, logvar_prior)
+            kld_loss = torch.mean(kld_loss)
+
+            std = torch.exp(0.5 * logvar_posterior)
+            z = eps * std + mean_posterior
+        return z, kld_loss
+
+
+def gaussian_kld(recog_mu, recog_logvar, prior_mu, prior_logvar):
+    kld = -0.5 * torch.sum(1 + (recog_logvar - prior_logvar)
+                           - torch.div(torch.pow(prior_mu - recog_mu,
+                                                 2), torch.exp(prior_logvar))
+                           - torch.div(torch.exp(recog_logvar), torch.exp(prior_logvar)), dim=-1)
+    return kld
+
+
 class LatentNorm(nn.Module):
     def __init__(self, args):
         super(LatentNorm, self).__init__()
--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from layers import ImageTransformerEncoder, LatentNorm
+from layers import ImageTransformerEncoder, Latent, LatentNorm
 from torch import nn
 import torch
 from transformers import BertModel, BertLMHeadModel
@@ -24,14 +24,17 @@ class VQGModel(nn.Module):
 
         if self.args.variant.split("-")[0] in ("icod", "icodqa"):
             self.text_encoder = BertModel.from_pretrained("bert-base-uncased", return_dict=True)
-        if self.args.variant == "icodqaf-icodqaf" or self.args.variant == "icodqaf-icof":
+        if self.args.variant in ("icodqaf-icodqaf", "icodqaf-icof"):
             self.text_encoder = BertModel.from_pretrained("bert-base-uncased", return_dict=True)
             self.multi_encoder = BertModel.from_pretrained("bert-base-uncased", return_dict=True)
             self.image_transformer = ImageTransformerEncoder(args)
+        if self.args.variant == "icod-icod-l,lg,lv":
+            self.text_encoder = BertModel.from_pretrained("bert-base-uncased", return_dict=True)
+            self.generative_encoder = BertModel.from_pretrained("bert-base-uncased", return_dict=True)
+            self.latent_layer = Latent(args)
 
         self.decoder = BertLMHeadModel.from_pretrained('bert-base-uncased', is_decoder=True, use_cache=True, add_cross_attention=True)
         self.latent_transformer = latent_transformer
-        self.latent = LatentNorm(args)
 
     def switch_latent_transformer(self, new_mode):
         self.latent_transformer = new_mode
@@ -57,6 +60,16 @@ class VQGModel(nn.Module):
         encoder_outputs = self.text_encoder(inputs_embeds=encoder_inputs, position_ids=encoder_pos_ids, attention_mask=encoder_attention_mask)
         encoder_hidden_states = encoder_outputs.last_hidden_state  # [B, T+1, D]
 
+        kld = None
+
+        # We'll manually construct the target embeddings, because in the case of a latent variable, we want to add some features to the embedding
+        target_pos_ids_single = torch.arange(question_ids.shape[-1]).to(self.args.device)  # [T_q]
+        target_pos_ids_batch = target_pos_ids_single.repeat(bsz, 1)  # [B, T_q]
+        target_embedding = self.embeddings(
+            input_ids=question_ids,
+            position_ids=target_pos_ids_batch
+        )  # [B, T_q, D]
+
         if self.args.variant == "icodqaf-icodqaf" or self.args.variant == "icodqaf-icof":
             encoded_objects, _ = self.image_transformer(obj_features, obj_locations)  # [B, 36, D]
             encoded_objects = encoded_objects.permute(1, 0, 2)
@@ -64,14 +77,14 @@ class VQGModel(nn.Module):
             encoder_attention_mask = torch.cat((encoder_attention_mask, object_mask), dim=1)
             encoder_hidden_states = torch.cat((encoder_hidden_states, encoded_objects), dim=1)
 
-        kld = None
-        if self.latent_transformer:
-            encoder_hidden_states, kld = self.latent(encoder_hidden_states)
-            # encoder_hidden_states[:, 0] = encoder_hidden_states[:, 0] + z  # Why doesn't this work??
-            # first_encoder_hidden_state = encoder_hidden_states[:, 0] + z
-            # encoder_hidden_states = torch.cat((first_encoder_hidden_state.unsqueeze(1), encoder_hidden_states[:, 1:]), dim=1)
+        if self.args.variant == "icod-icod-l,lg,lv":
+            generative_outputs = self.generative_encoder(input_ids=question_ids, attention_mask=question_attention_masks)
+            generative_hidden_states = generative_outputs.last_hidden_state  # [B, T_q, D]
+            if self.latent_transformer:
+                z, kld = self.latent_layer(encoder_hidden_states[:, 0], generative_hidden_states[:, 0])
+                target_embedding[:, 0] = target_embedding[:, 0] + z
 
-        outputs = self.decoder(input_ids=question_ids, labels=question_ids, attention_mask=question_attention_masks,
+        outputs = self.decoder(inputs_embeds=target_embedding, labels=question_ids, attention_mask=question_attention_masks,
                                encoder_hidden_states=encoder_hidden_states, encoder_attention_mask=encoder_attention_mask)
         loss = outputs["loss"]
 
@@ -105,17 +118,24 @@ class VQGModel(nn.Module):
             encoder_attention_mask = torch.cat((encoder_attention_mask, object_mask), dim=1)
             encoder_hidden_states = torch.cat((encoder_hidden_states, encoded_objects), dim=1)
 
-        kld = None
-        if self.latent_transformer:
-            encoder_hidden_states, kld = self.latent(encoder_hidden_states)
-            # encoder_hidden_states[:, 0] = encoder_hidden_states[:, 0] + z  # Why doesn't this work??
-            # first_encoder_hidden_state = encoder_hidden_states[:, 0] + z
-            # encoder_hidden_states = torch.cat((first_encoder_hidden_state.unsqueeze(1), encoder_hidden_states[:, 1:]), dim=1)
+        z, kld = torch.tensor(0).to(self.args.device), None
+        if self.args.variant == "icod-icod-l,lg,lv":
+            if self.latent_transformer:
+                z, kld = self.latent_layer(encoder_hidden_states[:, 0])
 
         ys = torch.ones(images.shape[0], 1).fill_(self.tokenizer.cls_token_id).long().to(self.args.device)
 
         for i in range(max_decode_len):
-            output = self.decoder(input_ids=ys, encoder_hidden_states=encoder_hidden_states, encoder_attention_mask=encoder_attention_mask)
+            target_pos_ids_single = torch.arange(ys.shape[-1]).to(self.args.device)  # [T_ys]
+            target_pos_ids_batch = target_pos_ids_single.repeat(bsz, 1)  # [B, T_ys]
+            target_embedding = self.embeddings(
+                input_ids=ys,
+                position_ids=target_pos_ids_batch
+            )  # [B, T_q, D]
+
+            target_embedding[:, 0] = target_embedding[:, 0] + z
+
+            output = self.decoder(inputs_embeds=target_embedding, encoder_hidden_states=encoder_hidden_states, encoder_attention_mask=encoder_attention_mask)
             logits = output["logits"]
             token = torch.argmax(logits[:, -1], dim=1)
 
